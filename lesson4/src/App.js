4051efdc6362ccaa97a850ac5d8a7f57bf92d18f
Oleg Soshko <ososhko@loginet.ru>
2020-03-18 10:40:09 +0300

    lesson8

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -2,25 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const divStyle = {
+    textAlign: 'center'
+  }
  return (
-   <div>
-      <div className="App">
-        <p>Hello World!</p>
+      <div style={divStyle}>
+        <h1 style={{color: 'blue', fontSize: '20px'}}>Hello World!</h1>
       </div>
-      <p>Hello</p>
-    </div>
   );
- 
-/*  return React.createElement(
-   'div',
-   {
-     className: 'App'
-   },
-   React.createElement(
-     'h1',
-     null,
-     'Hello world!'
-   ),
- ) */
 }
 export default App;
