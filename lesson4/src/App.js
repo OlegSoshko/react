f304d0aa1b1a95f5470b846374646df441a9d37f
Oleg Soshko <ososhko@loginet.ru>
2020-03-18 10:26:33 +0300

    lesson6

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
+ return (
     <div className="App">
       <p>Hello World!</p>
     </div>
   );
+ 
+/*  return React.createElement(
+   'div',
+   {
+     className: 'App'
+   },
+   React.createElement(
+     'h1',
+     null,
+     'Hello world!'
+   ),
+ ) */
 }
 export default App;
