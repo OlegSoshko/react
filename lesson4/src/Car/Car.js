d10401e45caea8a15bc00e6ae67bef18b1b8002a
Oleg Soshko <ososhko@loginet.ru>
2020-03-18 11:13:31 +0300

    lesson10: вывод динамических данных

--- lesson4/src/Car/Car.js
+++ lesson4/src/Car/Car.js
@@ -1,27 +1,10 @@
 import React from 'react';
 
-/* function Car(){
-    return(
-        <div>This is car component</div>
-    )
-} */
-
-/* const car = () =>{
-    return(
-        <div>This is car component.</div>
-    )
-} */
-
-/* const car = () => (
-    <div>
-        This is car component.
-        <strong>test</strong>
-    </div>
-    
-) */
-
 export default () => (
     <div>
-        This is component.
+        <p>This is component Car.</p>
+        <p>
+            Number: <strong>{Math.round(Math.random() * 100)}</strong>
+        </p>
     </div>
 )
